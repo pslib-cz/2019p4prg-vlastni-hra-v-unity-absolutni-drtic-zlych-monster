b1e0404e49cb0691b42ee72a4df9b6d3c2097902
Pavel Loubek <pavel.loubek@pslib.cz>
2019-12-12 01:51:04 +0100

    Everything

---
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    private Rigidbody2D rigbody;    // "Fyzika" kulky
+    public float speed = 20;        // private a public poté vyměním až přidám itemy
+    public int damage = 1;          // poškození
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigbody = GetComponent<Rigidbody2D>();          // dynamicky najde na komponentě (kulce) rigidbody
+        rigbody.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)     // použije se při kolizi s jiným colliderem
+    {
+        EnemyLogic enemy = collision.GetComponent<EnemyLogic>();
+        if (enemy != null)
+        {
+            enemy.EnemyTakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    public Transform bulletSpawn;       // určené místo pro spawnutí kulky
+    public GameObject bulletPrefab;     // co se bude spawnovat
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))       // při zmáčknutí lmb
+        {
+            ShootWeapon();
+        }
+    }
+
+    void ShootWeapon()
+    {
+        Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);      // naklonuj prefab kulky na pozici a rotaci místa pro spawnutí
+    }
+}
